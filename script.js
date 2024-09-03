bc142b9ff0110c67c95f2795995682859609e230
tintunaung124 <atintun100@gmail.com>
2024-09-03 15:00:09 +0630

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const listCotainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listCotainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listCotainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data, listCotainer.innerHTML");
+}
+function showTask() {
+  listCotainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
